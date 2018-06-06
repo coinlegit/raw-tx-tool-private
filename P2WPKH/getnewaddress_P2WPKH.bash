f1c984f3183a2f6aadbfdbd9589e9f028b31fa7e
yamada <oukinshu@gmail.com>
2018-06-06 22:32:24 +0900

    add bitcoin-cli getnewaddress

---
+++ P2WPKH/getnewaddress_P2WPKH.bash
@@ -0,0 +1,15 @@
+$ bash cli.bash  getnewaddress \"\" "bech32"
+tb1qx4h86l7v6pu3c8clkwm92el3u3qtrs7eygrayf
+
+$ bash cli.bash  validateaddress tb1qx4h86l7v6pu3c8clkwm92el3u3qtrs7eygrayf
+{
+  "isvalid": true,
+  "address": "tb1qx4h86l7v6pu3c8clkwm92el3u3qtrs7eygrayf",
+  "scriptPubKey": "0014356e7d7fccd0791c1f1fb3b65567f1e440b1c3d9",
+  "isscript": false,
+  "iswitness": true,
+  "witness_version": 0,
+  "witness_program": "356e7d7fccd0791c1f1fb3b65567f1e440b1c3d9"
+}
+
+
