cf89729bbf537b7954da8640302ae29b0294922b
yamada <oukinshu@gmail.com>
2018-06-17 23:18:03 +0900

    add signrawtransaction error

--- P2SH/signrawtransaction_P2SH.bash
+++ P2SH/signrawtransaction_P2SH.bash
@@ -1,4 +1,7 @@
 # redeem pay to P2SH
+#the bitcoin core not support sign this tx
+#it show ["error": "Unable to sign input, invalid stack size (possibly missing key)"]
+
 tx_id=94b231abe83d4b54d7179e122b5b712a2395bd24a14c4e5a31812ea22d127bd1
 send_2_addr=mjRbyRrWh9MF6NWy9pij2XguvsrAv3EvnE
 hex=$(bash cli.bash createrawtransaction "[{\"txid\":\"${tx_id}\",\"vout\":0}]" "{\"$send_2_addr\":1.2999}")
@@ -12,6 +15,5 @@ hex=$(bash cli.bash signrawtransaction "$hex" "[{\"txid\":\"$tx_id\",\"vout\":0,
 echo $hex
 
 #curl -X POST -d "tx_hex=${hex}" https://chain.so/api/v2/send_tx/BTCTEST
-
 #create txid=f6d4d14735ea283b0d3c8414c33283686ddcf0b01016616a4bf5aefcdbfc81f2
 
