bab2ffdaecd1907f8f6ec6041dd3b1376fb61f4e
yamada <oukinshu@gmail.com>
2019-02-01 16:12:26 +0900

    add some stuff

--- P2-MULTI-SIG/create_multisig_addr.bash
+++ P2-MULTI-SIG/create_multisig_addr.bash
@@ -1,5 +1,9 @@
+# redeem pay to P2SH-multisig
 #create P2 script multisig address
 
-bash cli.bash getnewaddress
-bash cli.bash getnewaddress
+addr1=$(bash cli.bash getnewaddress)
+addr2=$(bash cli.bash getnewaddress)
+pub_key1=$(bash cli.bash getaddressinfo $addr1 | jq -r '.pubkey')
+pub_key2=$(bash cli.bash getaddressinfo $addr2 | jq -r '.pubkey')
+bash cli.bash createmultisig 2 "[\"$pub_key1\",\"$pub_key2\"]"
 
