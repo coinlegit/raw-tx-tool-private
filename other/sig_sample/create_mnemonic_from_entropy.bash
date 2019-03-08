dad86c714b6c6740c7727dfa23dfd929e4d1db4f
passionofvc <yamada.passion@gmail.com>
2019-03-08 12:30:27 +0900

    add mnemonic sample

---
+++ other/sig_sample/create_mnemonic_from_entropy.bash
@@ -0,0 +1,9 @@
+#Info: Create a mnemonic seed (BIP39) from entropy. WARNING: mnemonic
+#should be created from properly generated entropy.
+#bx mnemonic-new aadf00dbaadf00dbaadf00dbaadf00d
+#16bytes entropy
+bx seed -b 128 | bx mnemonic-new --language es
+bx seed -b 128 | bx mnemonic-new --language ja
+
+
+
